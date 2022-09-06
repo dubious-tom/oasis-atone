osmosis-labs/osmosis

commit 3da88cf94ee9a963cd55ce22a3f8cf7a3c36f235

Small simulator backend cleanup (#2617)
* Move db code to be managed on its own

* Start removing tb.FailNow() in exchange for normal error handling

* Move header construction to mock_tendermint

* Remove an unhelpful events logging

* remove an arg

* wip

* import SQL from file

* add comment

simulation/executor/stats_db.go
@@ -1,0 +1,59 @@
+package simulation
+
+import (
+	"database/sql"
+	"embed"
+	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
+
+	"github.com/cosmos/cosmos-sdk/types/simulation"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+)
+
+//go:embed schema.sql
+var embedFs embed.FS
+
+// TODO: Setup an sql schema file instead of doing it in-line here
+type statsDb struct {
+	enabled bool
+	db      *sql.DB
+}
+
+func setupStatsDb(config ExportConfig) (statsDb, error) {
+	if !config.WriteStatsToDB {
+		return statsDb{enabled: false}, nil
+	}
+
+	setupSqlCmd, err := embedFs.ReadFile("schema.sql")
+	if err != nil {
+		return statsDb{}, fmt.Errorf("error in reading schema.sql: %w", err)
+	}
+
+	db, err := sql.Open("sqlite3", "./blocks.db")
+	if err != nil {
+		return statsDb{}, err
+	}
+
+	if _, err := db.Exec(string(setupSqlCmd)); err != nil {
+		db.Close()
+		return statsDb{}, fmt.Errorf("error in init from schema.sql init: %w", err)
+	}
+
+	return statsDb{enabled: true, db: db}, nil
+}
+
+func (stats statsDb) cleanup() {
+	stats.db.Close()
+}
+
+func (stats statsDb) logActionResult(header tmproto.Header, opMsg simulation.OperationMsg, resultData []byte) error {
+	if !stats.enabled {
+		return nil
+	}
+	appHash := fmt.Sprintf("%X", header.AppHash)
+	resData := fmt.Sprintf("%X", resultData)
+	sts := "INSERT INTO blocks(height,module,name,comment,passed, gasWanted, gasUsed, msg, resData, appHash) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10);"
+	_, err := stats.db.Exec(sts, header.Height, opMsg.Route, opMsg.Name, opMsg.Comment, opMsg.OK, opMsg.GasWanted, opMsg.GasUsed, opMsg.Msg, resData, appHash)
+	return err
+}

simulation/executor/mock_tendermint.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"math/rand"
 	"sort"
-	"testing"
 	"time"
 
 	"github.com/cosmos/cosmos-sdk/types/simulation"
@@ -12,6 +11,7 @@ import (
 	"github.com/tendermint/tendermint/crypto"
 	cryptoenc "github.com/tendermint/tendermint/crypto/encoding"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	tmtypes "github.com/tendermint/tendermint/types"
 	"golang.org/x/exp/maps"
 
 	markov "github.com/osmosis-labs/osmosis/v11/simulation/simtypes/transitionmatrix"
@@ -82,23 +82,53 @@ func (mv mockValidators) randomProposer(r *rand.Rand) crypto.PubKey {
 	return pk
 }
 
+func (mv mockValidators) toTmProtoValidators(proposerPubKey crypto.PubKey) (tmtypes.ValidatorSet, error) {
+	var tmProtoValSet tmproto.ValidatorSet
+	var tmTypesValSet *tmtypes.ValidatorSet
+	// iterate through current validators and add them to the TM ValidatorSet struct
+	for _, key := range mv.getKeys() {
+		var validator tmproto.Validator
+		mapVal := mv[key]
+		validator.PubKey = mapVal.val.PubKey
+		currentPubKey, err := cryptoenc.PubKeyFromProto(mapVal.val.PubKey)
+		if err != nil {
+			return *tmTypesValSet, err
+		}
+		validator.Address = currentPubKey.Address()
+		tmProtoValSet.Validators = append(tmProtoValSet.Validators, &validator)
+	}
+
+	// set the proposer chosen earlier as the validator set block proposer
+	var proposerVal tmtypes.Validator
+	proposerVal.PubKey = proposerPubKey
+	proposerVal.Address = proposerPubKey.Address()
+	blockProposer, err := proposerVal.ToProto()
+	if err != nil {
+		return *tmTypesValSet, err
+	}
+	tmProtoValSet.Proposer = blockProposer
+
+	// create a validatorSet type from the tmproto created earlier
+	tmTypesValSet, err = tmtypes.ValidatorSetFromProto(&tmProtoValSet)
+	return *tmTypesValSet, err
+}
+
 // updateValidators mimics Tendermint's update logic.
 func updateValidators(
-	tb testing.TB,
 	r *rand.Rand,
 	params simulation.Params,
 	current map[string]mockValidator,
 	updates []abci.ValidatorUpdate,
 	// logWriter LogWriter,
 	event func(route, op, evResult string),
-) map[string]mockValidator {
+) (map[string]mockValidator, error) {
 	nextSet := mockValidators(current).Clone()
 	for _, update := range updates {
 		str := fmt.Sprintf("%X", update.PubKey.GetEd25519())
 
 		if update.Power == 0 {
 			if _, ok := nextSet[str]; !ok {
-				tb.Fatalf("tried to delete a nonexistent validator: %s", str)
+				return nil, fmt.Errorf("tried to delete a nonexistent validator: %s", str)
 			}
 
 			// logWriter.AddEntry(NewOperationEntry())("kicked", str)
@@ -118,7 +148,7 @@ func updateValidators(
 		}
 	}
 
-	return nextSet
+	return nextSet, nil
 }
 
 // RandomRequestBeginBlock generates a list of signing validators according to
@@ -134,7 +164,7 @@ func RandomRequestBeginBlock(r *rand.Rand, params Params,
 		}
 	}
 
-	voteInfos := randomVoteInfos(r, params, validators, event)
+	voteInfos := randomVoteInfos(r, params, validators)
 	evidence := randomDoubleSignEvidence(r, params, validators, pastTimes, pastVoteInfos, event, header, voteInfos)
 
 	return abci.RequestBeginBlock{
@@ -146,7 +176,7 @@ func RandomRequestBeginBlock(r *rand.Rand, params Params,
 	}
 }
 
-func randomVoteInfos(r *rand.Rand, simParams Params, validators mockValidators, event func(route, op, evResult string),
+func randomVoteInfos(r *rand.Rand, simParams Params, validators mockValidators,
 ) []abci.VoteInfo {
 	voteInfos := make([]abci.VoteInfo, len(validators))
 
@@ -165,11 +195,7 @@ func randomVoteInfos(r *rand.Rand, simParams Params, validators mockValidators, 
 			signed = false
 		}
 
-		if signed {
-			event("begin_block", "signing", "signed")
-		} else {
-			event("begin_block", "signing", "missed")
-		}
+		// TODO: Do we want to log any data to statsdb here?
 
 		pubkey, err := cryptoenc.PubKeyFromProto(mVal.val.PubKey)
 		if err != nil {

simulation/executor/schema.sql
@@ -1,0 +1,14 @@
+DROP TABLE IF EXISTS blocks;
+-- TODO: Restructure into multiple tables, to have better encapsulation of block, action log, begin/end block, etc.
+CREATE TABLE blocks (
+    id INTEGER PRIMARY KEY,
+    height INT,
+    module TEXT, 
+    name TEXT, 
+    comment TEXT, 
+    passed BOOL, 
+    gasWanted INT, 
+    gasUsed INT, 
+    msg STRING, 
+    resData STRING, 
+    appHash STRING);

simulation/executor/simulate.go
@@ -1,7 +1,6 @@
 package simulation
 
 import (
-	"database/sql"
 	"fmt"
 	"io"
 	"math/rand"
@@ -16,7 +15,6 @@ import (
 
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 
-	_ "github.com/mattn/go-sqlite3"
 	abci "github.com/tendermint/tendermint/abci/types"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
@@ -53,24 +51,11 @@ func SimulateFromSeed(
 	actions := app.SimulationManager().Actions(config.Seed, app.AppCodec())
 
 	// Set up sql table
-	// TODO: Move all SQL stuff to its own file/package, should not be here.
-	var db *sql.DB
-	if config.ExportConfig.WriteStatsToDB {
-		db, err = sql.Open("sqlite3", "./blocks.db")
-		if err != nil {
-			tb.Fatal(err)
-		}
-		defer db.Close()
-		sts := `
-		DROP TABLE IF EXISTS blocks;
-		CREATE TABLE blocks (id INTEGER PRIMARY KEY, height INT,module TEXT, name TEXT, comment TEXT, passed BOOL, gasWanted INT, gasUsed INT, msg STRING, resData STRING, appHash STRING);
-		`
-		_, err := db.Exec(sts)
-
-		if err != nil {
-			tb.Fatal(err)
-		}
+	statsDb, err := setupStatsDb(config.ExportConfig)
+	if err != nil {
+		tb.Fatal(err)
 	}
+	defer statsDb.cleanup()
 
 	// Encapsulate the bizarre initialization logic that must be cleaned.
 	simCtx, simState, simParams, err := cursedInitializationLogic(tb, w, app, initFunctions, &config)
@@ -90,27 +75,26 @@ func SimulateFromSeed(
 	}()
 
 	testingMode, _, b := getTestingMode(tb)
-	blockSimulator := createBlockSimulator(testingMode, w, simParams, actions, simState, config, db)
+	blockSimulator := createBlockSimulator(testingMode, w, simParams, actions, simState, config, statsDb)
 
 	if !testingMode {
 		b.ResetTimer()
-	} else {
-		// recover logs in case of panic
-		defer func() {
-			if r := recover(); r != nil {
-				// TODO: Come back and cleanup the entire panic recovery logging.
-				// printPanicRecoveryError(r)
-				_, _ = fmt.Fprintf(w, "simulation halted due to panic on block %d\n", simState.header.Height)
-				simState.logWriter.PrintLogs()
-				panic(r)
-			}
-		}()
 	}
+	// recover logs in case of panic
+	defer func() {
+		if r := recover(); r != nil {
+			// TODO: Come back and cleanup the entire panic recovery logging.
+			// printPanicRecoveryError(r)
+			_, _ = fmt.Fprintf(w, "simulation halted due to panic on block %d\n", simState.header.Height)
+			simState.logWriter.PrintLogs()
+			panic(r)
+		}
+	}()
 
-	stopEarly = simState.SimulateAllBlocks(w, simCtx, blockSimulator)
+	stopEarly, err = simState.SimulateAllBlocks(w, simCtx, blockSimulator)
 
 	simState.eventStats.exportEvents(config.ExportConfig.ExportStatsPath, w)
-	return storetypes.CommitID{}, stopEarly, nil
+	return storetypes.CommitID{}, stopEarly, err
 }
 
 func simulationHomeDir() string {
@@ -224,12 +208,12 @@ func printPanicRecoveryError(recoveryError interface{}) {
 	fmt.Println("stack trace: " + errStackTrace)
 }
 
-type blockSimFn func(simCtx *simtypes.SimCtx, ctx sdk.Context, header tmproto.Header) (opCount int)
+type blockSimFn func(simCtx *simtypes.SimCtx, ctx sdk.Context, header tmproto.Header) (opCount int, err error)
 
 // Returns a function to simulate blocks. Written like this to avoid constant
 // parameters being passed everytime, to minimize memory overhead.
 func createBlockSimulator(testingMode bool, w io.Writer, params Params, actions []simtypes.ActionsWithMetadata,
-	simState *simState, config Config, db *sql.DB,
+	simState *simState, config Config, stats statsDb,
 ) blockSimFn {
 	lastBlockSizeState := 0 // state for [4 * uniform distribution]
 	blocksize := 0
@@ -237,7 +221,7 @@ func createBlockSimulator(testingMode bool, w io.Writer, params Params, actions 
 
 	return func(
 		simCtx *simtypes.SimCtx, ctx sdk.Context, header tmproto.Header,
-	) (opCount int) {
+	) (opCount int, err error) {
 		_, _ = fmt.Fprintf(
 			w, "\rSimulating... block %d/%d, operation 0/%d.",
 			header.Height, config.NumBlocks, blocksize,
@@ -262,7 +246,11 @@ func createBlockSimulator(testingMode bool, w io.Writer, params Params, actions 
 			opMsg.Route = action.ModuleName
 			cleanup()
 
-			simState.logActionResult(header, i, config, blocksize, opMsg, resultData, db, err)
+			err = simState.logActionResult(header, i, opMsg, resultData, stats, err)
+			if err != nil {
+				return opCount, fmt.Errorf("error on block  %d/%d, operation (%d/%d): %w",
+					header.Height, config.NumBlocks, i, blocksize, err)
+			}
 
 			simState.queueOperations(futureOps)
 
@@ -272,23 +260,18 @@ func createBlockSimulator(testingMode bool, w io.Writer, params Params, actions 
 			}
 		}
 
-		return blocksize
+		return blocksize, nil
 	}
 }
 
 // This is inheriting old functionality. We should break this as part of making logging be usable / make sense.
 func (simState *simState) logActionResult(
-	header tmproto.Header, actionIndex int, config Config, blocksize int,
-	opMsg simulation.OperationMsg, resultData []byte, db *sql.DB, actionErr error) {
+	header tmproto.Header, actionIndex int,
+	opMsg simulation.OperationMsg, resultData []byte, stats statsDb, actionErr error) error {
 	opMsg.LogEvent(simState.eventStats.Tally)
-	if config.ExportConfig.WriteStatsToDB {
-		appHash := fmt.Sprintf("%X", simState.header.AppHash)
-		resData := fmt.Sprintf("%X", resultData)
-		sts := "INSERT INTO blocks(height,module,name,comment,passed, gasWanted, gasUsed, msg, resData, appHash) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10);"
-		_, err := db.Exec(sts, header.Height, opMsg.Route, opMsg.Name, opMsg.Comment, opMsg.OK, opMsg.GasWanted, opMsg.GasUsed, opMsg.Msg, resData, appHash)
-		if err != nil {
-			simState.tb.Fatal(err)
-		}
+	err := stats.logActionResult(header, opMsg, resultData)
+	if err != nil {
+		return err
 	}
 
 	if !simState.config.Lean || opMsg.OK {
@@ -297,19 +280,19 @@ func (simState *simState) logActionResult(
 
 	if actionErr != nil {
 		simState.logWriter.PrintLogs()
-		simState.tb.Fatalf(`error on block  %d/%d, operation (%d/%d) from x/%s:
+		return fmt.Errorf(`error from x/%s:
 %v
-Comment: %s`,
-			header.Height, config.NumBlocks, actionIndex, blocksize, opMsg.Route, actionErr, opMsg.Comment)
+Comment: %s`, opMsg.Route, actionErr, opMsg.Comment)
 	}
+	return nil
 }
 
-// nolint: errcheck
-func (simState *simState) runQueuedOperations(simCtx *simtypes.SimCtx, ctx sdk.Context) (numOpsRan int) {
+// TODO: We need to cleanup queued operations, to instead make it queued action + have code re-use with prior code
+func (simState *simState) runQueuedOperations(simCtx *simtypes.SimCtx, ctx sdk.Context) (numOpsRan int, err error) {
 	height := int(simState.header.Height)
 	queuedOp, ok := simState.operationQueue[height]
 	if !ok {
-		return 0
+		return 0, nil
 	}
 
 	numOpsRan = len(queuedOp)
@@ -329,14 +312,13 @@ func (simState *simState) runQueuedOperations(simCtx *simtypes.SimCtx, ctx sdk.C
 
 		if err != nil {
 			simState.logWriter.PrintLogs()
-			simState.tb.Fatalf(`error on block  %d, height queued operation (%d/%d) from x/%s:
+			return 0, fmt.Errorf(`error on block  %d, height queued operation (%d/%d) from x/%s:
 %v
 Comment: %s`,
 				simState.header.Height, i, numOpsRan, opMsg.Route, err, opMsg.Comment)
-			simState.tb.FailNow()
 		}
 	}
 	delete(simState.operationQueue, height)
 
-	return numOpsRan
+	return numOpsRan, nil
 }

simulation/executor/simulate_dev.go
@@ -9,7 +9,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/simulation"
 	abci "github.com/tendermint/tendermint/abci/types"
 	"github.com/tendermint/tendermint/crypto"
-	cryptoenc "github.com/tendermint/tendermint/crypto/encoding"
 	"github.com/tendermint/tendermint/crypto/merkle"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	tmtypes "github.com/tendermint/tendermint/types"
@@ -31,10 +30,6 @@ type simState struct {
 
 	// block tx result data storage, reset after each block
 	Data [][]byte
-
-	// TODO: Figure out why we need this???
-	// Probably should get removed
-	tb testing.TB
 
 	// We technically have to store past block times for every block within the unbonding period.
 	// For simplicity, we take the RAM overhead and store all past times.
@@ -61,7 +56,6 @@ func newSimulatorState(simParams Params, initialHeader tmproto.Header, tb testin
 		operationQueue: NewOperationQueue(),
 		curValidators:  validators.Clone(),
 		nextValidators: validators.Clone(),
-		tb:             tb,
 		pastTimes:      []time.Time{},
 		pastVoteInfos:  [][]abci.VoteInfo{},
 		logWriter:      NewLogWriter(tb),
@@ -75,12 +69,12 @@ func newSimulatorState(simParams Params, initialHeader tmproto.Header, tb testin
 func (simState *simState) SimulateAllBlocks(
 	w io.Writer,
 	simCtx *simtypes.SimCtx,
-	blockSimulator blockSimFn) (stopEarly bool) {
+	blockSimulator blockSimFn) (stopEarly bool, err error) {
 	stopEarly = false
 	initialHeight := simState.config.InitializationConfig.InitialBlockHeight
 	numBlocks := simState.config.NumBlocks
 	for height := initialHeight; height < numBlocks+initialHeight && !stopEarly; height++ {
-		stopEarly = simState.SimulateBlock(simCtx, blockSimulator)
+		stopEarly, err = simState.SimulateBlock(simCtx, blockSimulator)
 		if stopEarly {
 			break
 		}
@@ -96,33 +90,41 @@ func (simState *simState) SimulateAllBlocks(
 		)
 		simState.logWriter.PrintLogs()
 	}
-	return stopEarly
+	return stopEarly, err
 }
 
 // simulate a block, update state
-func (simState *simState) SimulateBlock(simCtx *simtypes.SimCtx, blockSimulator blockSimFn) (stopEarly bool) {
+func (simState *simState) SimulateBlock(simCtx *simtypes.SimCtx, blockSimulator blockSimFn) (stopEarly bool, err error) {
 	if simState.header.ProposerAddress == nil {
 		fmt.Fprintf(simState.w, "\nSimulation stopped early as all validators have been unbonded; nobody left to propose a block!\n")
-		return true
+		return true, nil
 	}
 
 	requestBeginBlock := simState.beginBlock(simCtx)
 	ctx := simCtx.BaseApp().NewContext(false, simState.header)
 
 	// Run queued operations. Ignores blocksize if blocksize is too small
-	numQueuedOpsRan := simState.runQueuedOperations(simCtx, ctx)
+	numQueuedOpsRan, err := simState.runQueuedOperations(simCtx, ctx)
+	if err != nil {
+		return true, err
+	}
 	// numQueuedTimeOpsRan := simState.runQueuedTimeOperations(simCtx, ctx)
 
 	// run standard operations
 	// TODO: rename blockSimulator arg
-	operations := blockSimulator(simCtx, ctx, simState.header)
+	operations, err := blockSimulator(simCtx, ctx, simState.header)
 	simState.opCount += operations + numQueuedOpsRan // + numQueuedTimeOpsRan
+	if err != nil {
+		return true, err
+	}
 
 	responseEndBlock := simState.endBlock(simCtx)
 
-	simState.prepareNextSimState(simCtx, requestBeginBlock, responseEndBlock)
-
-	return false
+	err = simState.prepareNextSimState(simCtx, requestBeginBlock, responseEndBlock)
+	if err != nil {
+		return true, err
+	}
+	return false, nil
 }
 
 func (simState *simState) beginBlock(simCtx *simtypes.SimCtx) abci.RequestBeginBlock {
@@ -140,7 +142,7 @@ func (simState *simState) endBlock(simCtx *simtypes.SimCtx) abci.ResponseEndBloc
 	return res
 }
 
-func (simState *simState) prepareNextSimState(simCtx *simtypes.SimCtx, req abci.RequestBeginBlock, res abci.ResponseEndBlock) {
+func (simState *simState) prepareNextSimState(simCtx *simtypes.SimCtx, req abci.RequestBeginBlock, res abci.ResponseEndBlock) error {
 	// Log the current block's header time for future lookup
 	simState.pastTimes = append(simState.pastTimes, simState.header.Time)
 	simState.pastVoteInfos = append(simState.pastVoteInfos, req.LastCommitInfo.Votes)
@@ -159,7 +161,10 @@ func (simState *simState) prepareNextSimState(simCtx *simtypes.SimCtx, req abci.
 	proposerPubKey := simState.nextValidators.randomProposer(simCtx.GetRand())
 	simState.header.ProposerAddress = proposerPubKey.Address()
 	// find N + 2 valset
-	nPlus2Validators := updateValidators(simState.tb, simCtx.GetRand(), simState.simParams, simState.nextValidators, res.ValidatorUpdates, simState.eventStats.Tally)
+	nPlus2Validators, err := updateValidators(simCtx.GetRand(), simState.simParams, simState.nextValidators, res.ValidatorUpdates, simState.eventStats.Tally)
+	if err != nil {
+		return err
+	}
 
 	// now set variables in perspective of block n+1
 	simState.curValidators = simState.nextValidators
@@ -168,47 +173,22 @@ func (simState *simState) prepareNextSimState(simCtx *simtypes.SimCtx, req abci.
 	// utilize proposer public key and generate validator hash
 	// then, with completed block header, generate app hash
 	// see https://github.com/tendermint/tendermint/blob/v0.34.x/spec/core/data_structures.md#header for more info on block header hashes
-	simState.constructHeaderHashes(proposerPubKey)
+	return simState.constructHeaderHashes(proposerPubKey)
 }
 
-func (simState *simState) constructHeaderHashes(proposerPubKey crypto.PubKey) {
-	var currentValSet tmproto.ValidatorSet
-	// iterate through current validators and add them to the TM ValidatorSet struct
-	for _, key := range simState.curValidators.getKeys() {
-		var validator tmproto.Validator
-		mapVal := simState.curValidators[key]
-		validator.PubKey = mapVal.val.PubKey
-		currentPubKey, err := cryptoenc.PubKeyFromProto(mapVal.val.PubKey)
-		if err != nil {
-			panic(err)
-		}
-		validator.Address = currentPubKey.Address()
-		currentValSet.Validators = append(currentValSet.Validators, &validator)
-	}
-
-	// set the proposer chosen earlier as the validator set block proposer
-	var proposerVal tmtypes.Validator
-	proposerVal.PubKey = proposerPubKey
-	proposerVal.Address = proposerPubKey.Address()
-	blockProposer, err := proposerVal.ToProto()
+func (simState *simState) constructHeaderHashes(proposerPubKey crypto.PubKey) error {
+	currentValSet, err := simState.curValidators.toTmProtoValidators(proposerPubKey)
 	if err != nil {
-		panic(err)
-	}
-	currentValSet.Proposer = blockProposer
-
-	// create a validatorSet type from the tmproto created earlier
-	realValSet, err := tmtypes.ValidatorSetFromProto(&currentValSet)
-	if err != nil {
-		panic(err)
+		return err
 	}
 
 	// generate a hash from the validatorSet type and set it to the validators hash
-	simState.header.ValidatorsHash = realValSet.Hash()
+	simState.header.ValidatorsHash = currentValSet.Hash()
 
 	// create a header type from the tmproto
 	realHeader, err := tmtypes.HeaderFromProto(&simState.header)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	// TODO: We don't fill out the nextValidatorSet, we should eventually but not priority
@@ -223,4 +203,5 @@ func (simState *simState) constructHeaderHashes(proposerPubKey crypto.PubKey) {
 
 	// generate an apphash from this header and set this value
 	simState.header.AppHash = realHeader.Hash()
+	return nil
 }

dubious-tom/oasis-atone

commit 02e5a8b35c65f28744b4085923b3d64125aaa30c

fix(ci): simulator db close (#2624)
Closes: #XXX

## What is the purpose of the change

Fixing simulator breakage from: https://github.com/osmosis-labs/osmosis/pull/2617 to unblock CI

## Testing and Verifying

This change is a trivial rework / code cleanup without any test coverage.


## Documentation and Release Note

  - Does this pull request introduce a new feature or user-facing behavior changes? no
  - Is a relevant changelog entry added to the `Unreleased` section in `CHANGELOG.md`? no
  - How is the feature or change documented? not applicable

simulation/executor/stats_db.go
@@ -44,7 +44,9 @@ func setupStatsDb(config ExportConfig) (statsDb, error) {
 }
 
 func (stats statsDb) cleanup() {
-	stats.db.Close()
+	if stats.db != nil {
+		stats.db.Close()
+	}
 }
 
 func (stats statsDb) logActionResult(header tmproto.Header, opMsg simulation.OperationMsg, resultData []byte) error {